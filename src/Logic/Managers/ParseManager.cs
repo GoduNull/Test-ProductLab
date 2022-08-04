30cdfb7040e5823b7860032646c9b2e1ab12ee1c
Dzmitry Kaloska <61514386+GoduNull@users.noreply.github.com>
2022-08-04 21:44:14 +0300

    feat: change parsemanager

--- src/Logic/Managers/ParseManager.cs
+++ src/Logic/Managers/ParseManager.cs
@@ -10,10 +10,10 @@ namespace Logic.Managers
 {
     public class ParseManager
     {
-        HttpClient HttpClient = new HttpClient();
+        private HttpClient _httpClient = new HttpClient();
         public async Task<Root> GetRootAsync(string search)
         {
-            var response = await HttpClient.GetAsync($"https://search.wb.ru/exactmatch/ru/common/v4/search?appType=1&couponsGeo=12,3,18,15,21&curr=rub&dest=-1029256,-102269,-2162196,-1257786&emp=0&lang=ru&locale=ru&pricemarginCoeff=1.0&query={search}&reg=0&regions=68,64,83,4,38,80,33,70,82,86,75,30,69,22,66,31,40,1,48,71&resultset=catalog&sort=popular&spp=0&suppressSpellcheck=false");
+            var response = await _httpClient.GetAsync($"https://search.wb.ru/exactmatch/ru/common/v4/search?appType=1&couponsGeo=12,3,18,15,21&curr=rub&dest=-1029256,-102269,-2162196,-1257786&emp=0&lang=ru&locale=ru&pricemarginCoeff=1.0&query={search}&reg=0&regions=68,64,83,4,38,80,33,70,82,86,75,30,69,22,66,31,40,1,48,71&resultset=catalog&sort=popular&spp=0&suppressSpellcheck=false");
             return JsonConvert.DeserializeObject<Root>(
                await response.Content.ReadAsStringAsync());
         }
