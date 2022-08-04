6b98938a0f32e060674ace7f852dc7e720fbbff6
Dzmitry Kaloska <61514386+GoduNull@users.noreply.github.com>
2022-08-04 17:49:12 +0300

    feat: add models

---
+++ src/Data/Models/Product.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Models
+{
+    public class Product
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("brand")]
+        public string Brand { get; set; }
+
+        [JsonProperty("priceU")]
+        public int PriceU { get; set; }
+
+        [JsonProperty("feedbacks")]
+        public int Feedbacks { get; set; }
+    }
+}
---
+++ src/Data/Models/Root.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Models
+{
+    public class Root
+    {
+        [JsonProperty("data")]
+        public Data data { get; set; }
+    }
+}
